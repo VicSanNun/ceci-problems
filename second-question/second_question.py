327c34ee72003ab709391f225ad314687ae41b34
Victor Nunes <victornunes470@gmail.com>
2019-09-25 17:21:19 -0300

    Commenting second question

--- second-question/second_question.py
+++ second-question/second_question.py
@@ -1,15 +1,21 @@
-country = input('Insert Country: ').up per()
+#Recebendo a região do projeto.
+#O ".upper()" serve para deixar o texto em Maísculo. Desse modo, evito entradas como: bRasil ou Brasil ou brasil. transformo tudo em BRASIL
+country = input('Insert Country: ').upper()
+
+#Recebendo o orçamento
 budget = float(input('Insert Budget: '))
 
+#Verificando os Países e os orçamentos
 if (country == 'BRASIL'):
     if (budget <= 25000000):
+        #Exibo essa frase: "Total project value" e o valor do orçamento + porcentagem de margem de segurança
         print('Total project value: ', budget * 1.10)
     elif (budget > 25000000 and budget <= 33000000):
         print('Total project value: ', budget * 1.20)
     elif (budget > 33000000):
         print('Total project value: ', budget * 1.35)
 
-if (country == 'EUA'):
+elif (country == 'EUA'):
     if (budget <= 12500000):
         print('Total project value: ', budget * 1.10)
     elif (budget > 12500000 and budget <= 27000000):
@@ -17,10 +23,13 @@ if (country == 'EUA'):
     elif (budget > 27000000):
         print('Total project value: ', budget * 1.10)
 
-if (country == 'FRANCA' or country == 'FRANÇA'):
+elif (country == 'FRANCA' or country == 'FRANÇA'):
     if (budget <= 10000000):
         print('Total project value: ', budget * 1.10)
     elif (budget > 10000000 and budget <= 15000000):
         print('Total project value: ', budget * 1.10)
     elif (budget > 15000000):
         print('Total project value: ', budget * 1.10)
+
+else:
+    print('Please, restart the software and insert a valid Country !')
